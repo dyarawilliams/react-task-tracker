3c50c916d1df12a474afb37384e7abef5127f0e4
D'yara Williams <dyara.williams@gmail.com>
2021-06-03 21:18:11 -0400

    added event onclick and refactoring

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('CLICK')
+    }
+
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color="green" text="Add" />
-            <Button color="red" text="Delete" />
-            <Button color="" text="Edit" />
+            <Button color="#FF8E72" text="Add" onClick={onClick} />
         </header>
     )
 }
